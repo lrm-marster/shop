67ecb78e6554c9413d3f2f353159af3187a451da
lrm-marster <76636181+lrm-marster@users.noreply.github.com>
2021-02-22 23:23:09 +0800

    '1'

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-02-22 09:13:50
+ * @LastEditTime: 2021-02-22 20:14:03
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \shop-master\src\components\Login.vue
+-->
 <template>
     <div class="login_container">
         <div class="login_box">
@@ -7,24 +15,85 @@
             </div>
 
             <!-- 登录区域 -->
-            <el-form  label-width="0px" class="login_form">
-                <el-form-item>
-                    <el-input></el-input>
+            <el-form  label-width="0px" class="login_form" :model="loginform" :rules="rules" ref="loginform" status-icon>
+                <el-form-item prop="username">
+                    <el-input prefix-icon="iconfont icon-yonghu" v-model="loginform.username"></el-input>
                 </el-form-item>
-                <el-form-item>
-                    <el-input></el-input>
+                <el-form-item prop="password">
+                    <el-input prefix-icon="iconfont icon-mima" v-model="loginform.password" show-password></el-input>
                 </el-form-item>
                 <el-form-item class="btns">
-                    <el-button type="primary">确认</el-button>
-                    <el-button type="info">垂直</el-button>
+                    <el-button type="primary" @click="submitForm('loginform')">确认</el-button>
+                    <el-button type="info" @click="resetForm('loginform')">重置</el-button>
                 </el-form-item>
             </el-form>
+
+
         </div>
     </div>
 </template>
 
 <script>
 export default {
+  data(){
+    return {
+      // 这是一个表单绑定数据a
+      loginform: {
+        username:'',
+        password:''
+      },
+      rules:{
+        username:[
+          { required: true, message: '用户名不能为空', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        password:[
+          { required: true, message: '密码不能为空', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ]
+      }
+
+
+    }
+  },
+  methods:{
+    submitForm(formname){
+      this.$refs[formname].validate((valid) => {
+        if (valid) {
+          this.$http.get(
+            'login?name',
+            {
+              params:{
+                name: this.loginform.username,
+                password: this.loginform.password
+              }
+            }
+          ).then( res => {
+              console.log(res.data);
+              this.$message({
+                type:'success',
+                message:'登录成功'
+              });
+            }
+          ).catch( error => {
+            console.log(error );
+            this.$message.error('粗无');
+            window.localStorage.setItem('token','123456');
+            this.$router.push('/home');
+          });
+
+
+
+        } else {
+          // alert('验证不成功!');
+          return false;
+        }
+      });
+    },
+    resetForm(formname1){
+      this.$refs[formname1].resetFields();
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -6,6 +6,16 @@ import './plugins/element.js'
 
 // 导入全局样式
 import './assets/css/global.css'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+
+// 配置axios
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:85/'
+Vue.prototype.$http = axios
+
+import { Message } from 'element-ui'
+Vue.prototype.$message = Message
 
 Vue.config.productionTip = false
 
